e984a877c02eed27849c1d7d44d5c50a0bcd8652
kenna-hwa <76397046+kenna-hwa@users.noreply.github.com>
2021-08-15 16:28:30 +0900

    가져온 api 데이터 렌더링하기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import Movie from './Movie';
+
+import axios from 'axios';
 
 
 class App extends React.Component {
@@ -8,20 +10,30 @@ class App extends React.Component {
     isLoading: true,
     movies: [],
   }
+  getMovies = async () => {
+    const {data: {peopleInfoResult : {peopleInfo :{ filmos }}}} = await axios.get("https://kobis.or.kr/kobisopenapi/webservice/rest/people/searchPeopleInfo.json?key=8259f2a744e900c0fd98e8dc5b791507&peopleCd=10075724");
+    this.setState({movies : filmos, isLoading : false})
+  }
 
-componentDidMount(){
-  setTimeout(() => {
-    this.setState({isLoading : false})
-  }, 6000)
-}
+  componentDidMount(){
+    this.getMovies();
+  }
+  
   render(){
 
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
    
     return <div>
-      <p>{
-        isLoading ? "Loading..." : "We are Ready!"
-      }</p>
+      {
+        isLoading ? 
+        "Loading..." : 
+        movies.map ( movies=> {
+          return <Movie 
+          id={movies.movieCd} 
+          title={movies.movieNm} 
+          key={movies.movieCd} />
+        })
+      }
     </div>
   }
 }
